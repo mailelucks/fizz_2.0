fa9e61ffe9e27d4ce2b2147e0ba60be5be050cf0
Maile Lucks <maile.lucks@gmail.com>
2015-11-11 00:28:31 -0800

    Setting up the Prompt

---
+++ scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+var n = prompt("Enter a Number!");
+
+function fizzBuzz(n){
+	for (var i = 1; i <= n; i++) {
+		if ( (i % 3 === 0) && (i % 5 === 0) ){ 
+			var output = "FizzBuzz";
+			$('.list').append(output + "<br/>");
+		}
+		else if (i % 5 === 0) {
+			var output = "Buzz";
+			$('.list').append(output + "<br/>");
+		}
+		else if (i % 3 === 0) {
+			var output = "Fizz";
+			$('.list').append(output + "<br/>");
+		}
+		else {
+			var output = i;
+			$('.list').append(output + "<br/>");
+		}
+	};
+}
+
+fizzBuzz(n);
+
+});
