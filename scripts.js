842899a13ffec3bc0fc04c972d41578c31727b3f
Maile Lucks <maile.lucks@gmail.com>
2015-11-12 03:01:03 -0800

    Completed, updated debug script

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 
-var n = prompt("Enter a Number!");
-console.log(n);
-console.log(typeof n);
-var m = parseFloat(n);
-console.log(m);
-console.log(typeof m);
+var DEBUG_MODE = false;
+var debug = function(msg) {
+    if (DEBUG_MODE == true) {
+        console.log("DEBUG:", msg);
+    }
+}
 
-if ( (m % 1 === 0) && (typeof m === 'number') ){
-console.log("here");
-	function fizzBuzz(m){
-		// console.log("here");
+var n = prompt("Enter a Number!");
+debug(n);
+debug(typeof n);
+var m = parseFloat(n);
+debug(m);
+debug(typeof m);
+
+function fizzBuzz(m){
+debug("here1");
+	if (m % 1 === 0){
+		debug("here2");
 		for (var i = 1; i <= n; i++) {
+			debug("here3");
 			if ( (i % 3 === 0) && (i % 5 === 0) ){ 
 				var output = "FizzBuzz";
 				$('.list').append(output + "<br/>");
@@ -29,13 +37,12 @@ console.log("here");
 				$('.list').append(output + "<br/>");
 			}
 		}
-	}; 
-
-} 
-else {
-	
-	$('.list').append("Please refresh the page and enter a whole number with no decimals.");
-	
+	} 
+	else {
+		
+		$('.list').append("Please refresh the page and enter a whole number with no decimals.");
+		
+	};
 };
 
 fizzBuzz(n);
