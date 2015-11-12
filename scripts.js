a36720ed0be89a479bd93680f0ed45a7eddfa261
Maile Lucks <maile.lucks@gmail.com>
2015-11-12 02:04:08 -0800

    The string vs. number check almost works
    597a is being converted to a number and logs as 597, yet still fails
    the if statement. why is this?

--- scripts.js
+++ scripts.js
@@ -1,29 +1,40 @@
 $(document).ready(function() {
 
 var n = prompt("Enter a Number!");
-var m = parseInt("n", 16);
+console.log(n);
+console.log(typeof n);
+var m = parseInt(n, 10);
+console.log(m);
 console.log(typeof m);
 
-function fizzBuzz(m){
-	for (var i = 1; i <= n; i++) {
-		if ( (i % 3 === 0) && (i % 5 === 0) ){ 
-			var output = "FizzBuzz";
-			$('.list').append(output + "<br/>");
+if (typeof m !== 'number') {
+
+	function fizzBuzz(m){
+		for (var i = 1; i <= n; i++) {
+			if ( (i % 3 === 0) && (i % 5 === 0) ){ 
+				var output = "FizzBuzz";
+				$('.list').append(output + "<br/>");
+			}
+			else if (i % 5 === 0) {
+				var output = "Buzz";
+				$('.list').append(output + "<br/>");
+			}
+			else if (i % 3 === 0) {
+				var output = "Fizz";
+				$('.list').append(output + "<br/>");
+			}
+			else {
+				var output = i;
+				$('.list').append(output + "<br/>");
+			}
 		}
-		else if (i % 5 === 0) {
-			var output = "Buzz";
-			$('.list').append(output + "<br/>");
-		}
-		else if (i % 3 === 0) {
-			var output = "Fizz";
-			$('.list').append(output + "<br/>");
-		}
-		else {
-			var output = i;
-			$('.list').append(output + "<br/>");
-		}
-	};
-}
+	}; 
+
+} else {
+	
+	$('.list').append("Please refresh the page and enter a number.");
+	
+};
 
 fizzBuzz(n);
 
